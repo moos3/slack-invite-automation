2d4d482557961d601ed4f43a804b265bd5e09dcb
Richard Genther <richard.genthner@wheniwork.com>
2016-10-04 14:02:58 -0400

    my settings

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'GDG Tekirdağ',
+  community: process.env.COMMUNITY_NAME || 'Maine Hams',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'gdgtekirdag.slack.com',
+  slackUrl: process.env.SLACK_URL || 'mainehams.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   // You should generate the token in admin user, not owner.
@@ -12,9 +12,9 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-23716801746-23737858481-86350542804-05fd38befe46c235704ab489411ab2a6',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-86923716002-86902911076-87255683141-82baacddd3d0b06c83bac346d668b8ba',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
 
-  locale: process.env.LOCALE || "tr",
+  locale: process.env.LOCALE || "en",
 };
